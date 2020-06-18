cc0f03b4056c4ae522c46565e2fc91e640461972
richardsouthwell <richardsouthwell254@gmail.com>
2020-06-18 10:14:44 +0100

    linked with js and html

---
+++ src/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Applicative ((<|>))
+import Snap.Core (ifTop, route, writeText)
+import Snap.Http.Server (quickHttpServe)
+import Snap.Util.FileServe (serveDirectory, serveFile)
+
+main :: IO ()
+main = quickHttpServe $
+    ifTop (serveFile "views/index.html")
+    <|> route [ ("/static", serveDirectory "static")]
